3b1d58535cf535bbf1940dfc1b55390e7b3a5b11
m0rgause <antoresf1@gmail.com>
2023-06-15 08:43:31 +0700

    login and layouting

---
+++ app/Views/layout/template.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SuperBot</title>
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+</head>
+
+<body>
+    <?= $this->include('layout/navbar'); ?>
+    <?= $this->renderSection('content'); ?>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
